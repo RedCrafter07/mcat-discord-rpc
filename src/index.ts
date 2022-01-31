9cea5a6b0a694ca2b53b5acd4e47da3348a8a2d9
RedCrafter07 <github@redcrafter07.de>
2022-01-31 22:59:22 +0100

    Tray Icon Fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { app, BrowserWindow, Tray } from 'electron';
+import { app, BrowserWindow, Tray, Menu, NativeImage, nativeImage, Notification } from 'electron';
 import * as path from 'path';
+import rpc from "./lib/rpc";
 
 import socket from "./lib/socket";
 
-socket();
+const WebSocket = socket();
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -11,18 +12,32 @@ if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
+let tray: Tray | null;
+
 let mainWindow: BrowserWindow | null = null;
 
 const createWindow = (): void => {
   // Create the browser window.
-  if(mainWindow) return;
+  if(mainWindow) {
+    mainWindow.show();
+    return
+  }
   mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
-    resizable: false,
-    maximizable: false,
     darkTheme: true,
   });
+
+  mainWindow.on("close", () => {
+    mainWindow = null;
+    new Notification({
+      title: "Mcat-Dc",
+      body: "Mcat-Dc is still running in the background! Check your system tray to open it again or quit the app.",
+      silent: true,
+      timeoutType: "default",
+      subtitle: "Info"
+    }).show()
+  })
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, '../src/index.html'));
@@ -33,21 +48,17 @@ const createWindow = (): void => {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
+app.whenReady().then(() => {
+  createWindow();
+  runTray();
+});
+
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', (e: any) => {
+	e.preventDefault();
 });
-
-let quit = false;
-
-app.on("will-quit", (event) => {
-  if(quit == true) event.preventDefault();
-
-})
 
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
@@ -55,16 +66,28 @@ app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
-
-  console.log(`${__dirname}\\mcat.png`)
-
-  const tray = new Tray(`${__dirname}\\mcat.png`);
-  tray.setToolTip('Monstercat');
-  tray.on("click", () => {
-    if(mainWindow) mainWindow.show();
-    else createWindow();
-  })
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+function runTray() {
+  const icon = nativeImage.createFromPath(path.join(__dirname, "../src/mcat.png")).resize({ width: 32, height: 32 });
+	tray = new Tray(icon);
+	const contextMenu = Menu.buildFromTemplate([
+		{ label: 'Mcat-Dc', type: 'normal', enabled: false },
+		{
+			label: 'Quit',
+			type: 'normal',
+			click: () => {
+				app.quit();
+			}
+		}
+	]);
+
+  tray.on("click", () => {
+    createWindow();
+  })
+
+  tray.setContextMenu(contextMenu);
+}
