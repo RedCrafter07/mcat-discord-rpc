865a24aa73c364b01271cbb792b78136ed588087
RedCrafter07 <github@redcrafter07.de>
2022-01-31 20:47:10 +0100

    Database (config etc.)

---
+++ src/lib/db.ts
@@ -0,0 +1,11 @@
+import { JsonDB } from 'node-json-db';
+import { Config } from 'node-json-db/dist/lib/JsonDBConfig'
+
+// The first argument is the database filename. If no extension, '.json' is assumed and automatically added.
+// The second argument is used to tell the DB to save after each push
+// If you put false, you'll have to call the save() method.
+// The third argument is to ask JsonDB to save the database in an human readable format. (default false)
+// The last argument is the separator. By default it's slash (/)
+const db = new JsonDB(new Config("data", true, false, '/'));
+
+export default db;
