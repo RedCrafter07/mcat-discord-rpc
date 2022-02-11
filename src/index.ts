a5f56d72d7fc833bf85e2374f2cafda3fc9d2252
RedCrafter07 <github@redcrafter07.de>
2022-02-11 17:58:41 +0100

    Notification Icon

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
-import { app, BrowserWindow, Tray, Menu, NativeImage, nativeImage, Notification } from 'electron';
+import {
+	app,
+	BrowserWindow,
+	Tray,
+	Menu,
+	NativeImage,
+	nativeImage,
+	Notification,
+} from 'electron';
 import * as path from 'path';
-import rpc from "./lib/rpc";
+import rpc from './lib/rpc';
 
-import socket from "./lib/socket";
+import socket from './lib/socket';
 
 const WebSocket = socket();
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
-  // eslint-disable-line global-require
-  app.quit();
+	// eslint-disable-line global-require
+	app.quit();
 }
 
 let stopped = false;
@@ -19,34 +27,44 @@ let tray: Tray | null;
 let mainWindow: BrowserWindow | null = null;
 
 const createWindow = (): void => {
-  // Create the browser window.
-  if(mainWindow) {
-    mainWindow.show();
-    return
-  }
-  mainWindow = new BrowserWindow({
-    height: 600,
-    width: 800,
-    darkTheme: true,
-    backgroundColor: '#000000',
-  });
+	// Create the browser window.
+	if (mainWindow) {
+		mainWindow.show();
+		return;
+	}
+	mainWindow = new BrowserWindow({
+		height: 600,
+		width: 800,
+		darkTheme: true,
+		backgroundColor: '#000000',
+	});
 
-  mainWindow.setMenu(null);
+	mainWindow.setMenu(null);
 
-  mainWindow.on("close", () => {
-    mainWindow = null;
-    if(stopped == true) return;
-    new Notification({
-      title: "Mcat-Dc",
-      body: "Mcat-Dc is still running in the background! Check your system tray to open it again or quit the app.",
-      silent: true,
-      timeoutType: "default",
-      subtitle: "Info"
-    }).show()
-  })
+	mainWindow.on('close', () => {
+		mainWindow = null;
+		if (stopped == true) return;
+		new Notification({
+			title: 'Mcat-Dc',
+			body:
+				'Mcat-Dc is still running in the background! Check your system tray to open it again or quit the app.',
+			silent: true,
+			timeoutType: 'default',
+			subtitle: 'Info',
+			icon: path.join(__dirname, '../src/mcat.png'),
+		})
+			.on('action', (event, i) => {
+				if (i == 0) {
+					createWindow();
+				} else {
+					app.quit();
+				}
+			})
+			.show();
+	});
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, '../src/index.html'));
+	// and load the index.html of the app.
+	mainWindow.loadFile(path.join(__dirname, '../src/index.html'));
 };
 
 // This method will be called when Electron has finished
@@ -55,8 +73,8 @@ const createWindow = (): void => {
 app.on('ready', createWindow);
 
 app.whenReady().then(() => {
-  createWindow();
-  runTray();
+	createWindow();
+	runTray();
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
@@ -67,18 +85,20 @@ app.on('window-all-closed', (e: any) => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 
 function runTray() {
-  const icon = nativeImage.createFromPath(path.join(__dirname, "../src/mcat.png")).resize({ width: 32, height: 32 });
+	const icon = nativeImage
+		.createFromPath(path.join(__dirname, '../src/mcat.png'))
+		.resize({ width: 32, height: 32 });
 	tray = new Tray(icon);
 	const contextMenu = Menu.buildFromTemplate([
 		{ label: 'Mcat-Dc', type: 'normal', enabled: false },
@@ -86,15 +106,15 @@ function runTray() {
 			label: 'Quit',
 			type: 'normal',
 			click: () => {
-        stopped = true;
+				stopped = true;
 				app.quit();
-			}
-		}
+			},
+		},
 	]);
 
-  tray.on("click", () => {
-    createWindow();
-  })
+	tray.on('click', () => {
+		createWindow();
+	});
 
-  tray.setContextMenu(contextMenu);
+	tray.setContextMenu(contextMenu);
 }
