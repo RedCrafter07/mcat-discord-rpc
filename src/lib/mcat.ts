9df7271aff8e1926c6d1c9ed9c2a0e398426b939
RedCrafter07 <gh@r07.dev>
2022-11-11 14:40:40 +0100

    Update stuff

--- src/lib/mcat.ts
+++ src/lib/mcat.ts
@@ -1,38 +1,40 @@
-import {default as axios} from "axios";
-import db from "./db";
+import { default as axios } from 'axios';
+import db from './db';
 
 type CurrentlyPlaying = {
-    CurrentlyPlaying?: {
-      ReleaseId: string,  
-      TrackId: string,
-      UserId: string,
-      PlayTime: Date,
-      CurrentPlayLocation: number,
-      Duration: number,
-      TrackTitle: string,
-      TrackVersion: string,
-      ReleaseTitle: string,
-      ArtistsTitle: string,
-      CatalogId: string
-    },
-    playing: boolean
-}
+	CurrentlyPlaying?: {
+		ReleaseId: string;
+		TrackId: string;
+		UserId: string;
+		PlayTime: Date;
+		CurrentPlayLocation: number;
+		Duration: number;
+		TrackTitle: string;
+		TrackVersion: string;
+		ReleaseTitle: string;
+		ArtistsTitle: string;
+		CatalogId: string;
+	};
+	playing: boolean;
+};
 
 async function fetchCurrentSong(): Promise<CurrentlyPlaying> {
-    let secret = await db.getData("/secret");
-    if(!secret) return;
-    secret = secret.split("=")[1];
+	let secret = (await db.getData()).secret;
+	if (!secret || typeof secret != 'string') return;
+	secret = secret.split('=')[1];
 
-    let res;
-    try {
-        res = await axios.get(`https://player.monstercat.app/api/currently-playing?code=${secret}`)
-    }   catch(e) {
-        // console.log(e);
-    }
+	let res;
+	try {
+		res = await axios.get(
+			`https://player.monstercat.app/api/currently-playing?code=${secret}`
+		);
+	} catch (e) {
+		// console.log(e);
+	}
 
-    if(!res || !res.data) return {playing:false};
-    else res.data.playing = true;
-    return res.data;
+	if (!res || !res.data) return { playing: false };
+	else res.data.playing = true;
+	return res.data;
 }
 
 export default fetchCurrentSong;
